bd5d43eb1e3ef39ade1aa7c95dd9091b62c49548
Dima <vol.xin@mail.ru>
2022-12-18 00:58:18 +0300

    комментарий

---
+++ Example001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("видите число A ");
+Console.WriteLine("видите число B ");
+int numberA = int.Parse (Console.ReadLine());
+int numberB =7;
+Console.WriteLine ("numberA < numberB");
